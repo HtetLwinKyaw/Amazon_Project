b5be287fa6f160d895eb5536b18267d4f9f999ea
HtetLwinKyaw <htetlwinkyaw321@gmail.com>
2024-06-19 09:24:11 +0530

    save data and generate data for deliveryOption

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,7 +2,7 @@ import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-console.log(dayjs());
+
 
 
 let productsHTML= '';
